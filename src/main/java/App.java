3569fb32d0f16a335b161bd8c139c1bd2898322f
dikk13 <kuznecov77@mail.ru>
2022-06-29 14:53:09 +0300

    commit -a

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat bean2 = applicationContext.getBean(Cat.class);
+        System.out.println(bean1);
+        Cat bean3 = applicationContext.getBean(Cat.class);
+        System.out.println(bean1);
+        System.out.println(bean == bean1);
+        System.out.println(bean2 == bean3);
     }
 }
